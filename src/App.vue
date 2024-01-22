5c8e7257e8fabd20f42b09fe15a7121c70a7d3b6
GiordanoDiego <diegomgiordano96@gmail.com>
2024-01-22 15:27:43 +0100

    added axios

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
-// import axios form 'axios';
 import {store} from './store.js'
+import axios from 'axios';
 
 export default {
     data(){
@@ -24,6 +24,11 @@ export default {
     },
     methods: {
 
+    }, 
+    mounted(){
+        axios.get('https://rickandmortyapi.com/api/character?page=1').then((response) => {
+            console.log(response);
+        });
     }
 }
 
